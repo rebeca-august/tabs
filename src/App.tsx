04f1068f533e6be637c1159f4e0742ba0f7e4413
rebeca-august <rebeca.cazacu@gmail.com>
2021-02-17 15:13:25 +0000

    add typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 
+type Job = {
+  id: string
+  order: number
+  title: string
+  dates: string
+  duties: string[]
+  company: string
+}
+
 const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
   const [loading, setLoading] = useState(true)
-  const [jobs, setJobs] = useState([])
+  const [jobs, setJobs] = useState<Job[]>([])
   const [value, setValue] = useState(0)
 
   const fetchJobs = async () => {
     const response = await fetch(url)
     const newJobs = await response.json()
-    console.log(newJobs)
+
     setJobs(newJobs)
     setLoading(false)
   }
